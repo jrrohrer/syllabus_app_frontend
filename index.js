4ef25c1e551306c2bb3be15d301c1664e5b9a375
jrrohrer <jessie.rohrer@gmail.com>
2021-07-26 12:07:24 -0400

    connecting front end to api; GET implementation

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("in index.js");
+const endPoint = "http://localhost:3000/api/v1/syllabuses"
+
+document.addEventListener('DOMContentLoaded', () => {
+  getSyllabi();
+})
+
+function getSyllabi() {
+  fetch(endPoint)
+  .then(response => response.json())
+  .then(syllabi => {
+    syllabi.data.forEach(syllabus => {
+      const syllabusMarkup = `
+        <div data-id=${syllabus.id}>
+          <img src="${syllabus.attributes.image_url}" height="200" width="250">
+          <h3>${syllabus.attributes.title}</h3>
+          <p>${syllabus.attributes.category.name}</p>
+          <button data-id=${syllabus.id}>edit</button>
+        </div>
+        <br>`;
+
+        document.querySelector('#syllabus-container').innerHTML += syllabusMarkup;
+    })
+  })
+}
