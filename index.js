bf60b9a46ac11bb13a76252aa9b00cdb18321177
jrrohrer <jessie.rohrer@gmail.com>
2021-07-27 14:12:19 -0400

    add post fetch; category id bug fix

--- index.js
+++ index.js
@@ -30,13 +30,36 @@ function createFormHandler(e) {
   e.preventDefault();
   const titleInput = document.querySelector("#input-title").value;
   const descriptionInput = document.querySelector("#input-description").value;
-  const imageInput = document.querySelector("#input-image").value;
+  const imageInput = document.querySelector("#input-url").value;
   const categoryInput = document.querySelector("#categories").value;
   const categoryId = parseInt(categoryInput);
-  postFetch(titleInput, descriptionInput, imageInput, categoryId);
+  postSyllabus(titleInput, descriptionInput, imageInput, categoryId);
 }
 
-function postFetch(title, description, image_url, category_id) {
-  
+function postSyllabus(title, description, image_url, category_id) {
+  // build body object
+  let bodyData = {title, description, image_url, category_id}
+  fetch(endPoint, {
+    // POST request
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(bodyData)
+  })
+  .then(response => response.json())
+  .then(syllabus => { console.log(syllabus)
+    const syllabusData = syllabus.data.attributes
+    
+    // render JSON response
+    const syllabusMarkup = `
+      <div data-id=${syllabus.id}>
+        <img src=${syllabusData.image_url} height="200" width="250">
+        <h3>${syllabusData.title}</h3>
+        <p>${syllabusData.category.name}</p>
+        <button data-id=${syllabusData.id}>edit</button>
+      </div>
+      <br><br>`;
+
+    document.querySelector('#syllabus-container').innerHTML += syllabusMarkup;
+  })
 }
 
